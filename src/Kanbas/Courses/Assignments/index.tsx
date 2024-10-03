8b2a472d8427854a0788569a4d8b1c1cc07c0851
Rutvik Dhopate <rutvikdhopate@gmail.com>
2024-10-03 12:17:31 -0700

    Assignment Tab Margin Fix

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -18,7 +18,7 @@ export default function Assignments() {
                 <span className="float-end" style={{marginRight:"20px"}}><button style={{ borderRadius: "10px"}}>40% of Total</button></span>
             </div>
             <ul className="wd-assignment-list list-group rounded-0">
-                <li className="wd-assignment-list-item list-group-item p-3 ps-1">
+                <li className="wd-assignment-list-item list-group-item p-3 ps-1 wd-lesson">
                     <AssignmentControlButtons/>
                     <div style={{marginLeft:"75px"}}>
                         <a className="wd-assignment-link text-black"
@@ -38,40 +38,40 @@ export default function Assignments() {
             </ul>
 
             <ul className="wd-assignment-list list-group rounded-0">
-                <li className="wd-assignment-list-item list-group-item p-3 ps-1">
+                <li className="wd-assignment-list-item list-group-item p-3 ps-1 wd-lesson">
                     <AssignmentControlButtons/>
                     <div style={{marginLeft:"75px"}}>
                         <a className="wd-assignment-link text-black"
                             href="#/Kanbas/Courses/1234/Assignments/123"
                             style={{textDecoration:"none"}}>
-                            <strong>A1</strong>
+                            <strong>A2</strong>
                         </a>
                         <br/>
                         <span><a className="wd-modules-list-link text-danger"
-                            href="https://www.google.com" style={{textDecoration:"none"}}>Multiple Modules</a> | <strong>Not available until</strong> May 6 at 12:00 am | </span>
+                            href="https://www.google.com" style={{textDecoration:"none"}}>Multiple Modules</a> | <strong>Not available until</strong> May 13 at 12:00 am | </span>
                         <br />
                         <span>
-                            <strong>Due</strong> May 13 at 11:59 pm | 100 pts
+                            <strong>Due</strong> May 20 at 11:59 pm | 100 pts
                         </span>
                     </div>
                 </li>
             </ul>
 
             <ul className="wd-assignment-list list-group rounded-0">
-                <li className="wd-assignment-link list-group-item p-3 ps-1">
+                <li className="wd-assignment-link list-group-item p-3 ps-1 wd-lesson">
                     <AssignmentControlButtons/>
                     <div style={{marginLeft:"75px"}}>
                         <a className="wd-assignment-link text-black"
                             href="#/Kanbas/Courses/1234/Assignments/123"
                             style={{textDecoration:"none"}}>
-                            <strong>A1</strong>
+                            <strong>A3</strong>
                         </a>
                         <br/>
                         <span><a className="wd-modules-list-link text-danger"
-                            href="https://www.google.com" style={{textDecoration:"none"}}>Multiple Modules</a> | <strong>Not available until</strong> May 6 at 12:00 am | </span>
+                            href="https://www.google.com" style={{textDecoration:"none"}}>Multiple Modules</a> | <strong>Not available until</strong> May 20 at 12:00 am | </span>
                         <br />
                         <span>
-                            <strong>Due</strong> May 13 at 11:59 pm | 100 pts
+                            <strong>Due</strong> May 27 at 11:59 pm | 100 pts
                         </span>
                     </div>
                 </li>
